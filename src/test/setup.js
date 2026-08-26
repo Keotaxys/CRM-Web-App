8b94c9f89227e0becac4f33f2dad6b1974549991
keotaxys <keota.mar@gmail.com>
2026-08-26 18:54:59 +0700

    feat: add accessible custom select controls

--- src/test/setup.js
+++ src/test/setup.js
@@ -7,3 +7,16 @@ if (!globalThis.ResizeObserver) {
     disconnect() {}
   };
 }
+
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener() {},
+    removeListener() {},
+    addEventListener() {},
+    removeEventListener() {},
+    dispatchEvent() { return false; },
+  });
+}
